af856f7eb636374bea8816ff96c974703980eb19
root <root@foogene.(none)>
2013-11-19 20:20:38 +0000

    Added caption-toggling button for turning preview streaming on and off. (Not yet tied to script.)

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
+fileSuffix = "snap"; // This should get overwritten promptly, but just in case, we need some value
 
 var app = express();
 
@@ -44,6 +45,8 @@ server = http.createServer(app).listen(app.get('port'), function(){
   io.sockets.on('connection', function (socket) {
     cameraSettings = readCameraSettings(socket);
     socket.emit('cameraSettings', cameraSettings);
+    generalSettings = readGeneralSettings(socket);
+    socket.emit('generalSettings', generalSettings);
     socket.on('my other event', function (data) {
       console.log(data);
     });
@@ -62,6 +65,41 @@ server = http.createServer(app).listen(app.get('port'), function(){
   });
 });
 
+function sensitivityString() {
+  if (cameraSettings.sensitivity == "100") {return "--ISO 100 ";}
+  else if (cameraSettings.sensitivity == "200") {return "--ISO 200 ";}
+  else if (cameraSettings.sensitivity == "400") {return "--ISO 400 ";}
+}
+
+function sharpnessString() {
+  if (cameraSettings.sharpness == "natural") {return "";}
+  else if (cameraSettings.sharpness == "sharpened") {return "--sharpness 50 ";}
+}
+
+function contrastString() {
+  if (cameraSettings.contrast == "natural") {return "";}
+  else if (cameraSettings.contrast == "reduced") {return "--contrast 50 ";}
+  else if (cameraSettings.contrast == "increased") {return "--contrast -50 ";}
+}
+
+function saturationString() {
+  if (cameraSettings.saturation == "natural") {return "";}
+  else if (cameraSettings.saturation == "mutedColors") {return "--saturation 50 ";}
+  else if (cameraSettings.saturation == "vividColors") {return "--saturation -50 ";}
+}
+
+function brightnessString() {
+  if (cameraSettings.brightness == "natural") {return "";}
+  else if (cameraSettings.brightness == "dimmer") {return "--brightness 10 ";}
+  else if (cameraSettings.brightness == "brighter") {return "--brightness 100 ";}
+}
+
+function imageSizeString() {
+  if (cameraSettings.imageSize == "full") {return "";}
+  else if (cameraSettings.imageSize == "reduced800") {return "--width 800 --height 600 ";}
+  else if (cameraSettings.imageSize == "reduced1200") {return "--width 1200 --height 900 ";}
+}
+
 function snap () {
   var sys = require('sys')
   var exec = require('child_process').exec;
@@ -71,16 +109,29 @@ function snap () {
   function puts(error, stdout, stderr) { sys.puts(stdout) }
   var cameraSettings = readCameraSettings();
   // this os where to read the different camera settings and modify the command that follows.
-  exec("raspistill -o ./public/images/fullsnaps/"+filename_time_prefix+"snap.jpg", puts);
+  var cameraCommand = "raspistill ";
+  cameraCommand += sensitivityString();
+  cameraCommand += sharpnessString();
+  cameraCommand += contrastString();
+  cameraCommand += saturationString();
+  cameraCommand += brightnessString();
+  cameraCommand += imageSizeString();
+  cameraCommand += "-o ./public/images/fullsnaps/"+filename_time_prefix+"_"+fileSuffix+".jpg";
+  //exec("raspistill -o ./public/images/fullsnaps/"+filename_time_prefix+"_"+fileSuffix+".jpg", puts);
+  console.log(cameraCommand);
+  exec(cameraCommand, puts);
 }
 
-function writeCameraSettings (cameraSettings) {
+function writeCameraSettings (newCameraSettings) {
+  cameraSettings = newCameraSettings;
   var fd = fs.openSync("/var/www/cameraSettings.cfg", 'w', 0666);
   fs.writeSync(fd, JSON.stringify(cameraSettings));
   fs.closeSync(fd);
 }
 
-function writeGeneralSettings (generalSettings) {
+function writeGeneralSettings (newGeneralSettings) {
+  generalSettings = newGeneralSettings;
+  fileSuffix = generalSettings.fileSuffix;
   console.log("General Settings updated:");
   console.log(generalSettings);
   var fd = fs.openSync("/var/www/generalSettings.cfg", 'w', 0666);
@@ -89,21 +140,31 @@ function writeGeneralSettings (generalSettings) {
 }
 
 function readCameraSettings (socket) {
-  if (!fs.existsSync("/var/www/cameraSettings.cfg")) createDefaultConfigFile();
+  if (!fs.existsSync("/var/www/cameraSettings.cfg")) createDefaultCameraConfigFile();
   var cameraSettingsString = fs.readFileSync("/var/www/cameraSettings.cfg");
-  var cameraSettings = JSON.parse(cameraSettingsString);
+  cameraSettings = JSON.parse(cameraSettingsString);
   return cameraSettings;
 }
 
-function createDefaultConfigFile() {
+function readGeneralSettings (socket) {
+  if (!fs.existsSync("/var/www/generalSettings.cfg")) createDefaultGeneralConfigFile();
+  var generalSettingsString = fs.readFileSync("/var/www/generalSettings.cfg");
+  generalSettings = JSON.parse(generalSettingsString);
+  fileSuffix = generalSettings.fileSuffix;
+  return generalSettings;
+}
+
+function createDefaultCameraConfigFile() {
   var cameraSettings = '{"sensitivity":"100","sharpness":"natural","contrast":"natural","saturation":"natural","brightness":"natural","imageSize":"full"}';
   var fd = fs.openSync("/var/www/cameraSettings.cfg", 'w', 0666);
   fs.writeSync(fd, cameraSettings);
   fs.closeSync(fd);
+}
 
-  var generalSettings = '{"fileSuffix":"snap"}';
-  fd = fs.openSync("/var/www/generalSettings.cfg", 'w', 0666);
-  fs.writeSync(fd, cameraSettings);
+function createDefaultGeneralConfigFile() {
+  var generalSettings = '{"uploadSite":"dropbox","fileSuffix":"snap"}';
+  var fd = fs.openSync("/var/www/generalSettings.cfg", 'w', 0666);
+  fs.writeSync(fd, generalSettings);
   fs.closeSync(fd);
 }
 
